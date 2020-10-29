1a1c813463fc5eb0dc01f3e66df01d3afa6a5439
yoojinhee03 <yoojinhee03@gmail.com>
2020-10-29 14:03:39 +0900

    init

---
+++ src/main/java/com/example/homepi/HomepiApplication.java
@@ -0,0 +1,13 @@
+package com.example.homepi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomepiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomepiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homepi/HomepiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homepi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomepiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
